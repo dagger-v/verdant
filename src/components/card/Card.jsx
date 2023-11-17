6872db54e7a465ff61f52713214cdd9c45b69b0e
dagger-v <goingarcher@gmail.com>
2023-11-17 02:53:44 -0800

    added categories

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,23 +6,25 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src="/p1.jpeg" alt="" fill />
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.05.2023</span>
+          <span className={styles.date}>11.05.2023 - </span>
           <span className={styles.category}>STYLE</span>
         </div>
         <Link href="/">
           <h1> Lorem ipsum dolor sit, amet consectetur adipisicing elit. </h1>
         </Link>
-        <p>
+        <p className={styles.desc}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
           praesentium voluptatem beatae dolorem tenetur harum accusamus? Fuga
           eligendi sint possimus placeat similique inventore, illo asperiores
           culpa? Alias fugit quasi numquam! ...
         </p>
-        <Link href="/">Read More</Link>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
