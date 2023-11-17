7ed53fba6e14986999cf0a8e3f8f8f08e3d72978
dagger-v <goingarcher@gmail.com>
2023-11-17 01:56:06 -0800

    updated home page

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
 import styles from "./card.module.css";
+import Link from "next/link";
 
 const Card = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/p1.jpeg" alt="" fill />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.05.2023</span>
+          <span className={styles.category}>STYLE</span>
+        </div>
+        <Link href="/">
+          <h1> Lorem ipsum dolor sit, amet consectetur adipisicing elit. </h1>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+          praesentium voluptatem beatae dolorem tenetur harum accusamus? Fuga
+          eligendi sint possimus placeat similique inventore, illo asperiores
+          culpa? Alias fugit quasi numquam! ...
+        </p>
+        <Link href="/">Read More</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import styles from "./featured.module.css";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Verdant</h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </h1>
+          <p className={styles.Desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum modi
+            consequatur impedit aperiam fugiat? Soluta tenetur, similique ipsa,
+            animi vero porro fugit at adipisci eveniet, consequatur asperiores
+            expedita eligendi ducimus? Excepturi quaerat eveniet harum
+            laudantium illum inventore consequatur quam rerum explicabo beatae.
+            Fuga architecto accusantium eius magni illum quibusdam labore eum
+            delectus fugiat totam! Delectus, minus. Debitis nam eaque
+            accusantium.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
